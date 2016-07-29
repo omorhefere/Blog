fb86deb0dd166a9ad1ecd9593b4c417d19369ec4
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-07-29 19:24:03 +0100

    Chart added

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -1,59 +1,9 @@
-$.ajax({
-           type: "GET",
-           contentType: "application/json; charset=utf-8",
-           url: 'me/data',
-           dataType: 'json',
-           success: function (data) {
-               draw(data);
-           },
-           error: function (result) {
-               error();
-           }
-       });
+$(function() {
+  $("#bars li .bar").each( function( key, bar ) {
+    var percentage = $(this).data('percentage');
 
-function draw(data) {
-    // the colour
-    var color = d3.scale.category20b();
-    //the width of the chart
-    var width = 800,
-        barHeight = 20; // the height of the bar.
-    var language = ["Vanilla Javascrript", "Python", "Ruby on Rails", "Node Js", "Angular 2"];
-
-    var x = d3.scale.linear()
-        .range([0, width])
-        .domain([0, 5]);
-
-    var chart = d3.select("#graph")
-        .attr("width", width)
-        .attr("height", barHeight * data.length);
-
-    var bar = chart.selectAll("g")
-        .data(data)
-        .enter().append("g")
-        .attr("transform", function (d, i) {
-                  return "translate(0," + i * barHeight + ")";
-              });
-
-    bar.append("rect")
-        .attr("width", x)
-        .attr("height", barHeight - 1)
-
-        .style("fill", function (d) {
-                   return color(d);
-               })
-
-    bar.append("text")
-        .attr("x", function (d) {
-                  return 10;
-              })
-        .attr("y", barHeight / 2)
-        .attr("dy", ".35em")
-        .style("fill", "white")
-        .text(function (d) {
-                  return d.key;
-              });
-}
-
-function error() {
-    console.log("error")
-}
+    $(this).animate({
+      'height' : percentage + '%'
+    }, 1000);
+  });
+});
--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -3,13 +3,7 @@ class MeController < ApplicationController
 
   end
 
-  def data
-    respond_to do |format|
-      format.json {
-        render :json => [ 1 ,2,3,4,5]
-      }
-    end
-  end
+  
 
 
 end
